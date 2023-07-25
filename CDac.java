68a215b158239906297f9e0abefe44e72de5d011
saket40 <vikaskatakwar94@gmail.com>
2023-07-25 16:20:06 +0530

    second commit

--- CDac.java
+++ CDac.java
@@ -1,5 +1,6 @@
 public class CDac{
     public static void main(String[] args) {
     System.out.println("I am studing in c-Dac");
+    System.out.println("I am preparing for the DAC course");
     }
 }
